766dc4dcc124d3dfd686b0d9882d6b48ecaa0547
Rai-19 <hamingja.205@gmail.com>
2025-05-21 19:10:11 +0900

    sms

--- page-notice.php
+++ page-notice.php
@@ -15,34 +15,19 @@
   <div class="display">
     <div class="news-list">
       <div class="news section" id="section1">
-        <blockquote class="twitter-tweet">
-          <p lang="ja" dir="ltr">動くたびに猫が揺れるようにしたんだけど、動画の撮り方が下手w<br>私の技術的にこれが限界w伝わるんだろうかw <a href="https://t.co/pdhF6UWPxo">pic.twitter.com/pdhF6UWPxo</a></p>&mdash; Rai (@rai_0311) <a href="https://twitter.com/rai_0311/status/1833018137652191740?ref_src=twsrc%5Etfw">September 9, 2024</a>
-        </blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php the_content() ?>
       </div>
       <div class="news section" id="section2">
-        <blockquote class="twitter-tweet">
-          <p lang="ja" dir="ltr">半々ピアスw <a href="https://t.co/rzGiF9Vofa">pic.twitter.com/rzGiF9Vofa</a></p>&mdash; Rai (@rai_0311) <a href="https://twitter.com/rai_0311/status/1830813698132451512?ref_src=twsrc%5Etfw">September 3, 2024</a>
-        </blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php the_content() ?>
       </div>
       <div class="news section" id="section3">
-        <blockquote class="twitter-tweet">
-          <p lang="ja" dir="ltr">気付いたら写真撮るのにもこだわるようになってしまった <a href="https://t.co/SOkZKjHg9z">pic.twitter.com/SOkZKjHg9z</a></p>&mdash; Rai (@rai_0311) <a href="https://twitter.com/rai_0311/status/1830437982563975249?ref_src=twsrc%5Etfw">September 2, 2024</a>
-        </blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php the_content() ?>
       </div>
       <div class="news section" id="section4">
-        <blockquote class="twitter-tweet">
-          <p lang="ja" dir="ltr">黒猫のリクエストもらったので作ってみました♪<br>まず猫に見えるのか問題w <a href="https://t.co/F1562z227S">pic.twitter.com/F1562z227S</a></p>&mdash; Rai (@rai_0311) <a href="https://twitter.com/rai_0311/status/1825817740524806453?ref_src=twsrc%5Etfw">August 20, 2024</a>
-        </blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php the_content() ?>
       </div>
       <div class="news section" id="section5">
-        <blockquote class="twitter-tweet">
-          <p lang="ja" dir="ltr">久しぶりに実家帰ったら箱入り娘がいた <a href="https://t.co/GerdCleoaW">pic.twitter.com/GerdCleoaW</a></p>&mdash; Rai (@rai_0311) <a href="https://twitter.com/rai_0311/status/1825480452909801862?ref_src=twsrc%5Etfw">August 19, 2024</a>
-        </blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <?php the_content() ?>
       </div>
     </div>
   </div>
